48805c3dd2f955c5958b8007f27d9d41aaa26d20
yitzhak <matias6@gmail.com>
2025-01-29 01:47:59 -0400

    add data

---
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,47 @@
+﻿using MovieManagementAppServices.Core.Entities;
+using MovieManagementAppServices.Core.Interfaces;
+using System;
+
+namespace MovieManagementAppServices.Infrastructure.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly AppDbContext _context;
+
+        public MovieRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Movie>> GetAllMovies() =>
+            await _context.Movies.Include(m => m.Actors).Include(m => m.Ratings).ToListAsync();
+
+        public async Task<Movie> GetMovieById(int id) =>
+            await _context.Movies.Include(m => m.Actors).Include(m => m.Ratings)
+                                 .FirstOrDefaultAsync(m => m.Id == id);
+
+        public async Task AddMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateMovie(Movie movie)
+        {
+            _context.Movies.Update(movie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteMovie(int id)
+        {
+            var movie = await GetMovieById(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+
+}
